09b227c25cc373247a021b740d9c70088c4347e5
Mikhail Kotelnikov <mikhail.kotelnikov@ubimix.com>
2014-11-09 12:06:28 +0100

    XML parsing was moved in a separate method

--- src/KmlDataProvider.js
+++ src/KmlDataProvider.js
@@ -12,21 +12,27 @@ var KmlDataProvider = DataProvider.extend({
         return Mosaic.P(dataSets);
     },
     loadDatasetEntities : function(dataset, callback) {
+        var that = this;
         var dataFolder = this._getDataFolder();
         var fileName = Path.join(dataFolder, dataset.path);
         return this._downloadDataSet(dataset, fileName).then(
                 function() {
                     return Mosaic.P.ninvoke(FS, 'readFile', fileName, 'UTF-8')
-                        .then(function(str) {
-                            var doc = jsdom(str);
-                            var result = tj.kml(doc, { styles: true });
-                            var list = _.isArray(result) //
-                            ? result : _.isArray(result.features) //
-                            ? result.features : [ result ];
-                            _.each(list, callback);
-                        })
+                            .then(function(str) {
+                                var doc = that._convertToXml(str);
+                                var result = tj.kml(doc, {
+                                    styles : true
+                                });
+                                var list = _.isArray(result) //
+                                ? result : _.isArray(result.features) //
+                                ? result.features : [ result ];
+                                _.each(list, callback);
+                            })
                 })
     },
+    _convertToXml : function(str) {
+        return jsdom(str);
+    }
 });
 
 module.exports = KmlDataProvider;
